f4326740993bd189cb34cf576e591d6c0f975913
LeandroBiazao <leandro@developer2x.com>
2025-08-10 18:09:54 -0500

    Add users with signals computed values

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header";
+import { UserComponent } from "./user/user";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, UserComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
