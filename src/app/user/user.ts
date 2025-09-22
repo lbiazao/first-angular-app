53979bf1254a9c676a6cb8e39bbcea7a4b4038b2
LeandroBiazao <leandro@developer2x.com>
2025-09-22 17:51:22 -0500

    Commiting lates changes for app

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -10,8 +10,8 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class UserComponent {
   @Input({ required: true}) id!: string;
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  @Input({ required: true}) avatar!: string;
+  @Input({ required: true}) name!: string;
   @Output() select = new EventEmitter<string>();
 
 
