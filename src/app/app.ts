e9f50ab1cf70c2d7c206d41c05d1e5194ff0e805
LeandroBiazao <leandro@developer2x.com>
2025-09-22 18:14:09 -0500

    If Statement

--- src/app/app.ts
+++ src/app/app.ts
@@ -14,7 +14,7 @@ import { TasksComponent } from './tasks/tasks';
 })
 export class App {
   users = DUMMY_USERS;
-  selectedUserID = 'u1';
+  selectedUserID: string | undefined;
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserID)!;
